461b24c702f95bc46b1b25bd22ff19360a3fc4c2
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-03-10 21:20:58 +0100

    commit 2
    Ujradolgoztam pár dolgot. És változtattam a menün.

--- src/project0307/java0307.java
+++ src/project0307/java0307.java
@@ -3,95 +3,130 @@ package project0307;
 import java.util.Random;
 import java.util.Scanner;
 
-    class Muveletek{
-        boolean osszeadas() {
-            Random rnd = new Random();
-            int szam1 = rnd.nextInt(101);
-            int szam2 = rnd.nextInt(101-szam1);
-            Scanner sc = new Scanner(System.in);
-            System.out.printf("%d+%d=", szam1, szam2);
-            int valasz=sc.nextInt();
-            int eredmeny=szam1+szam2;
-            if (eredmeny==valasz) {
-                System.out.println("Helyes válasz!");
-                return true;
-            }else{
-                System.out.println("Rossz válasz! A helyes válasz: "+eredmeny);
-                return false;
-            }
-        }
-        boolean kivonas() {
-            Random rnd = new Random();
-            int szam1 = rnd.nextInt(101);
-            int szam2 = rnd.nextInt(szam1);
-            Scanner sc = new Scanner(System.in);
-            System.out.printf("%d-%d=", szam1, szam2);
-            int valasz=sc.nextInt();
-            int eredmeny=szam1-szam2;
-            if (eredmeny==valasz) {
-                System.out.println("Helyes válasz!");
-                return true;
-            }else{
-                System.out.println("Rossz válasz! A helyes válasz: "+eredmeny);
-                return false;
-            }
-            
-        }
-        void szorzas() {
-            
-            Random rnd = new Random();
-            int szam1 = rnd.nextInt(10);
-            int szam2 = rnd.nextInt(10);
-            Scanner sc = new Scanner(System.in);
-            System.out.println(szam1 +"+"+ szam2 +"=__");
-            int valasz=sc.nextInt();
-            int eredmeny=szam1+szam2;
-            if (eredmeny == valasz) {
-                System.out.println("Helyes válasz!");
-             
-            }else{
-                System.out.println("Rossz válasz! A helyes válasz: "+eredmeny);
-               
-        }  
-        
-        }
-        void osztas() {
-        
+class Muveletek {
+    boolean osszeadas() {
+        Random rnd = new Random();
+        int szam1 = rnd.nextInt(100) + 1;
+        int szam2 = rnd.nextInt(100 - szam1) + 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d+%d=", szam1, szam2);
+        int valasz = sc.nextInt();
+        int eredmeny = szam1 + szam2;
+        if (eredmeny == valasz) {
+            System.out.println("Helyes válasz!");
+            return true;
+        } else {
+            System.out.println("Rossz válasz! A helyes válasz: " + eredmeny);
+            return false;
         }
     }
+
+    boolean kivonas() {
+        Random rnd = new Random();
+        int szam1 = rnd.nextInt(100) + 1;
+        int szam2 = rnd.nextInt(szam1);
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d-%d=", szam1, szam2);
+        int valasz = sc.nextInt();
+        int eredmeny = szam1 - szam2;
+        if (eredmeny == valasz) {
+            System.out.println("Helyes válasz!");
+            return true;
+        } else {
+            System.out.println("Rossz válasz! A helyes válasz: " + eredmeny);
+            return false;
+        }
+
+    }
+
+    boolean szorzas() {
+
+        Random rnd = new Random();
+        int szam1 = rnd.nextInt(10) + 1;
+        int szam2 = rnd.nextInt(10) + 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d*%d=", szam1, szam2);
+        int valasz = sc.nextInt();
+        int eredmeny = szam1 + szam2;
+        if (eredmeny == valasz) {
+            System.out.println("Helyes válasz!");
+            return true;
+        } else {
+            System.out.println("Rossz válasz! A helyes válasz: " + eredmeny);
+            return false;
+
+        }
+
+    }
+
+    boolean osztas() {
+        Random rnd = new Random();
+        int szam1 = rnd.nextInt(10) + 1;
+        int szam2 = rnd.nextInt(10) + 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d/%d=", (szam1 * szam2), szam2);
+        int valasz = sc.nextInt();
+        int eredmeny = (szam1 * szam2) / szam2;
+        if (eredmeny == valasz) {
+            System.out.println("Helyes válasz!");
+            return true;
+        } else {
+            System.out.println("Rossz válasz! A helyes válasz: " + eredmeny);
+            return false;
+        }
+    }
+}
 
 public class java0307 {
 
     public static void main(String[] args) {
-        int helyes=0, rossz=0;
+        int helyes = 0, rossz = 0;
         boolean joE;
         int muvelet;
         Scanner sc = new Scanner(System.in);
         Muveletek mu = new Muveletek();
-        do {            
-            System.out.println("Milyen műveletet szeretnél gyakorolni?(Számot adj meg!)\n 1.Összeadás\n 2.Kivonás\n 3. Szorzás\n 4.Osztás");
-            muvelet=sc.nextInt();
-        } while (!(1<=muvelet && muvelet<=4));
-        
-        if (muvelet==1) {
-            joE=mu.osszeadas();
-            if (joE) {
-                helyes++;
-            }else{
-                rossz++;
+        while (true) {
+
+            do {
+                System.out.printf("Helyes válaszok:%d Helytelen válaszok:%d\n", helyes, rossz);
+                System.out.println(
+                        "Milyen műveletet szeretnél gyakorolni?(Számot adj meg!)\n 1.Összeadás\n 2.Kivonás\n 3.Szorzás\n 4.Osztás\n\n 5.Kilépés");
+                System.out.print("Válasz:");
+                muvelet = sc.nextInt();
+            } while (!(1 <= muvelet && muvelet <= 5));
+
+            if (muvelet == 1) {
+                joE = mu.osszeadas();
+                if (joE) {
+                    helyes++;
+                } else {
+                    rossz++;
+                }
+            } else if (muvelet == 2) {
+                joE = mu.kivonas();
+                if (joE) {
+                    helyes++;
+                } else {
+                    rossz++;
+                }
+            } else if (muvelet == 3) {
+                joE = mu.szorzas();
+                if (joE) {
+                    helyes++;
+                } else {
+                    rossz++;
+                }
+            } else if (muvelet == 4) {
+                joE = mu.osztas();
+                if (joE) {
+                    helyes++;
+                } else {
+                    rossz++;
+                }
+            } else if (muvelet == 5) {
+                System.out.println("Köszi hogy játszottál!");
+                return;
             }
-            //System.out.println(helyes+" "+rossz);
-        } else if(muvelet==2) {
-            joE=mu.kivonas();
-            if (joE) {
-                helyes++;
-            }else {
-                rossz++;
-            }
-        } else if(muvelet==3){
-            mu.szorzas();
-        } else if(muvelet==4){
-            mu.osztas();
         }
     }
 }
