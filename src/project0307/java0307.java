774ac7576c36a338e25857098ad837cca602a169
Kovács Milán <95681544+Milkacola@users.noreply.github.com>
2022-03-10 19:53:38 +0100

    Update java0307.java
    kivonás hozzáadása

--- src/project0307/java0307.java
+++ src/project0307/java0307.java
@@ -13,12 +13,28 @@ import java.util.Scanner;
             int valasz=sc.nextInt();
             int eredmeny=szam1+szam2;
             if (eredmeny==valasz) {
+                System.out.println("Helyes válasz!");
                 return true;
             }else{
+                System.out.println("Rossz válasz! A helyes válasz: "+eredmeny);
                 return false;
             }
         }
-        void kivonas() {
+        boolean kivonas() {
+            Random rnd = new Random();
+            int szam1 = rnd.nextInt(101);
+            int szam2 = rnd.nextInt(szam1);
+            Scanner sc = new Scanner(System.in);
+            System.out.printf("%d-%d=", szam1, szam2);
+            int valasz=sc.nextInt();
+            int eredmeny=szam1-szam2;
+            if (eredmeny==valasz) {
+                System.out.println("Helyes válasz!");
+                return true;
+            }else{
+                System.out.println("Rossz válasz! A helyes válasz: "+eredmeny);
+                return false;
+            }
             
         }
         void szorzas() {
@@ -51,12 +67,16 @@ public class java0307 {
             }
             //System.out.println(helyes+" "+rossz);
         } else if(muvelet==2) {
-            mu.kivonas();
+            joE=mu.kivonas();
+            if (joE) {
+                helyes++;
+            }else {
+                rossz++;
+            }
         } else if(muvelet==3){
             mu.szorzas();
         } else if(muvelet==4){
             mu.osztas();
         }
-        
     }
 }
