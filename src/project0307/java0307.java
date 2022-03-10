8948e902a0612609b7096684edeacfa8c652d40c
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-03-10 18:37:14 +0100

    osszeadas metódus KÉSZ

--- src/project0307/java0307.java
+++ src/project0307/java0307.java
@@ -1,10 +1,22 @@
 package project0307;
 
+import java.util.Random;
 import java.util.Scanner;
 
     class Muveletek{
-        void osszeadas() {
-            
+        boolean osszeadas() {
+            Random rnd = new Random();
+            int szam1 = rnd.nextInt(101);
+            int szam2 = rnd.nextInt(101-szam1);
+            Scanner sc = new Scanner(System.in);
+            System.out.printf("%d+%d=", szam1, szam2);
+            int valasz=sc.nextInt();
+            int eredmeny=szam1+szam2;
+            if (eredmeny==valasz) {
+                return true;
+            }else{
+                return false;
+            }
         }
         void kivonas() {
             
@@ -21,6 +33,7 @@ public class java0307 {
 
     public static void main(String[] args) {
         int helyes=0, rossz=0;
+        boolean joE;
         int muvelet;
         Scanner sc = new Scanner(System.in);
         Muveletek mu = new Muveletek();
@@ -30,7 +43,13 @@ public class java0307 {
         } while (!(1<=muvelet && muvelet<=4));
         
         if (muvelet==1) {
-            mu.osszeadas();
+            joE=mu.osszeadas();
+            if (joE) {
+                helyes++;
+            }else{
+                rossz++;
+            }
+            //System.out.println(helyes+" "+rossz);
         } else if(muvelet==2) {
             mu.kivonas();
         } else if(muvelet==3){
