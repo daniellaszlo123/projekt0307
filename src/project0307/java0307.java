eaaf64f4deeecf11ad65060ba854da6272a2f0fc
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-03-10 18:31:01 +0100

    Alap és metódusokra bontás

--- src/project0307/java0307.java
+++ src/project0307/java0307.java
@@ -2,20 +2,41 @@ package project0307;
 
 import java.util.Scanner;
 
+    class Muveletek{
+        void osszeadas() {
+            
+        }
+        void kivonas() {
+            
+        }
+        void szorzas() {
+        
+        }
+        void osztas() {
+        
+        }
+    }
+
 public class java0307 {
 
     public static void main(String[] args) {
-        int elso = 0, masodik = 0;
-        Scanner scan = new Scanner(System.in);
-
-        while (elso <= 0 || elso > 100) {
-
-            System.out.print("Add meg az első számot: ");
-            elso = scan.nextInt();
-        }
-        while (masodik <= 0 || masodik > 100) {
-            System.out.print("Add meg a második számot: ");
-            masodik = scan.nextInt();
+        int helyes=0, rossz=0;
+        int muvelet;
+        Scanner sc = new Scanner(System.in);
+        Muveletek mu = new Muveletek();
+        do {            
+            System.out.println("Milyen műveletet szeretnél gyakorolni?(Számot adj meg!)\n 1.Összeadás\n 2.Kivonás\n 3. Szorzás\n 4.Osztás");
+            muvelet=sc.nextInt();
+        } while (!(1<=muvelet && muvelet<=4));
+        
+        if (muvelet==1) {
+            mu.osszeadas();
+        } else if(muvelet==2) {
+            mu.kivonas();
+        } else if(muvelet==3){
+            mu.szorzas();
+        } else if(muvelet==4){
+            mu.osztas();
         }
         
     }
